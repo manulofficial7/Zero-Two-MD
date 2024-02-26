78027127bac4bb5ab83947a5844880ce10a3f64b
manulofficial7 <157870475+manulofficial7@users.noreply.github.com>
2024-02-26 20:14:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'youre session id',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'youre POSTGRESQL url',
 LANG: process.env.BOT_LANG || 'EN' ,
-ANTI_BAD: process.env.ANTI_BAD || 'false',
+ANTI_BAD: process.env.ANTI_BAD || 'true',
 MAX_SIZE: process.env.MAX_SIZE || 200,
-ONLY_GROUP: process.env.ONLY_GROUP || 'false',
-ANTI_LINK: process.env.ANTI_LINK || 'false' ,
+ONLY_GROUP: process.env.ONLY_GROUP || 'true',
+ANTI_LINK: process.env.ANTI_LINK || 'true' ,
 ANTI_BOT: process.env.ANTI_BOT || 'false',
 ALIVE: process.env.ALIVE || `default`,
-FOOTER: process.env.FOOTER ||  '©𝚉𝙴𝚁𝙾-𝚃𝚆𝙾 𝙼𝙳',
-LOGO: process.env.LOGO || `https://github.com/vihangayt0/ZeroTwo-Uploads/blob/main/ZERO-TWO.png?raw=true` 
+FOOTER: process.env.FOOTER ||  '©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️',
+LOGO: process.env.LOGO || `https://telegra.ph/file/2717975a4410096a24c0f.jpg` 
 };
